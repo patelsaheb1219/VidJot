570e84d6c0a62dfe786ed3d5be097b8e8c475ee3
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-10-26 22:57:54 +0530

    Code updated

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 const mongoose = require("mongoose");
 
 const app = express();
 
-//Load routes
+// Load routes
 const ideas = require("./routes/ideas");
 const users = require("./routes/users");
 
 // Passport Config
 require("./config/passport")(passport);
-//DB config
+// DB Config
 const db = require("./config/database");
 
-//Map global promise - get rid of warning
+// Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
-
-//Connect to mongoose
+// Connect to mongoose
 mongoose
   .connect(
     db.mongoURI,
     {
-      useNewUrlParser: true
+      useMongoClient: true
     }
   )
-  .then(() => {
-    console.log("MongoDB connected...");
-  })
+  .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
-//Handlebars Middleware
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// Handlebars Middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-// method-override middleware
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Method override middleware
 app.use(methodOverride("_method"));
 
-//Express-session middleware
+// Express session midleware
 app.use(
   session({
     secret: "secret",
@@ -51,13 +60,13 @@ app.use(
   })
 );
 
-//Passport middleware
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-//Global variables
+// Global variables
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
@@ -66,23 +75,25 @@ app.use(function(req, res, next) {
   next();
 });
 
-//BOdy-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Index Route
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", {
+    title: title
+  });
+});
 
-//static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-//About route
+// About Route
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-//use routes
+// Use routes
 app.use("/ideas", ideas);
 app.use("/users", users);
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`Server started on ${port}`);
+  console.log(`Server started on port ${port}`);
 });
